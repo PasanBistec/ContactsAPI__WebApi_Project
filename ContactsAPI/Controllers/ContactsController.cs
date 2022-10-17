58949dfd2ac660bfe75fed42ff1add6ec86400c1
PasanBistec <pasan@bistecglobal.com>
2022-10-17 10:06:39 +0530

    added the controller

--- ContactsAPI/Controllers/ContactsController.cs
+++ ContactsAPI/Controllers/ContactsController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ContactsAPI.Controllers
 {
+
+    [ApiController]
+    [Route("")]
     public class ContactsController : Controller
     {
-        public IActionResult Index()
+
+        // creating private property so can talk to db
+        private readonly ContactsAPIDbContext dbContext;
+
+        // create a constructer and inject the db cotect
+        public ContactsController (ContactsAPIDbContext dbContext)
         {
-            return View();
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetContacts()
+        {
+            return Ok(dbContext.Contacts.ToList());
         }
     }
 }
